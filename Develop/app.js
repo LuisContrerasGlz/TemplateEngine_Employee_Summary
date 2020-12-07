9b2ed6bfef81f840a84c5f06f1e2da8e8ed96d49
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2020-12-06 18:21:53 -0600

    created constructors classess and inquired

--- Develop/app.js
+++ Develop/app.js
@@ -1,7 +1,8 @@
+const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
+
 const path = require("path");
 const fs = require("fs");
 
@@ -10,6 +11,172 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+
+employees =[];
+
+function thisisend(){
+    console.log("termine");
+    console.log(employees);
+    render(employees);
+}
+
+function new_engineer(){
+    console.log("your select is an Engineer");
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "mameEngineer", 
+            message: "What is the Engineer's Name? "
+        },
+        {
+            type: "input",
+            name: "idEngineer", 
+            message: "What is the Engineer's ID? "
+        },
+        {
+            type: "input",
+            name: "emailEngineer", 
+            message: "What is your Engineer' email? "
+        },
+        {
+            type: "input",
+            name: "gitEngineer", 
+            message: "What is your Engineer's github username? "
+        },
+        {
+            type: "list",
+            name: "optMember", 
+            message: "Wich type of team member wold you like to add? ",
+            choices: ["Engineer","Intern","I don´t want to add any more team members"]
+        }
+    ])
+    .then(engineers => {
+        console.log('engineers');
+        let e = new Engineer.Engineer(engineers.mameEngineer,engineers.idEngineer, engineers.emailEngineer, engineers.gitEngineer);
+        employees.push(e);
+        //console.log(e.getName());
+        //console.log(e.getId());
+        //console.log(e.getEmail());
+        //console.log(e.getGithub());
+        //console.log(e.getRole());
+        if (engineers.optMember=="Engineer"){
+            new_engineer();
+        }else{
+            if (engineers.optMember=="Intern"){
+                new_intern();
+            }else{
+                thisisend();
+            }
+        
+        }
+
+    })
+}
+
+
+function new_intern(){
+    console.log("your select is an intern");
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "mameIntern", 
+            message: "What is the Intern's Name? "
+        },
+        {
+            type: "input",
+            name: "idIntern", 
+            message: "What is the Intern's ID? "
+        },
+        {
+            type: "input",
+            name: "emailIntern", 
+            message: "What is your Intern's email? "
+        },
+        {
+            type: "input",
+            name: "schoolIntern", 
+            message: "What is your Intern's school? "
+        },
+        {
+            type: "list",
+            name: "optMember", 
+            message: "Wich type of team member wold you like to add? ",
+            choices: ["Engineer","Intern","I don´t want to add any more team members"]
+        }
+    ])
+    .then(interns => {
+        console.log('interns');
+        let e = new Intern.Intern(interns.mameIntern,interns.idIntern, interns.emailIntern, interns.schoolIntern);
+        employees.push(e);
+        //console.log(e.getName());
+        //console.log(e.getId());
+        //console.log(e.getEmail());
+        //console.log(e.getSchool());
+        //console.log(e.getRole());
+        if (interns.optMember=="Engineer"){
+            new_engineer();
+        }else{
+            if (interns.optMember=="Intern"){
+                new_intern();
+            }else{
+                thisisend();
+            }
+        
+        }
+    })
+}
+
+
+
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "nameManager", 
+        message: "What is your Manager's Name? "
+    },
+    {
+        type: "input",
+        name: "idManager", 
+        message: "What is your Manager's ID? "
+    },
+    {
+        type: "input",
+        name: "emailManager", 
+        message: "What is your Manager's email? "
+    },
+    {
+        type: "input",
+        name: "oficceManager", 
+        message: "What is your Manager's Office Number? "
+    },
+    {
+        type: "list",
+        name: "optMember", 
+        message: "Wich type of team member wold you like to add? ",
+        choices: ["Engineer","Intern","I don´t want to add any more team members"]
+    }
+])
+.then(managers => {
+
+    console.log('managers');
+    let m = new Manager.Manager(managers.nameManager,managers.idManager, managers.emailManager, managers.oficceManager);
+    employees.push(m);
+    //console.log(m.getName());
+    //console.log(m.getId());
+    //console.log(m.getEmail());
+    //console.log(m.getOfficeNumber());
+    //console.log(m.getRole());
+    if (managers.optMember=="Engineer"){
+        new_engineer();
+    }else{
+        if (managers.optMember=="Intern"){
+            new_intern();
+        }else{
+            thisisend();
+        }
+    }
+})
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
